43c4c9af2196e9906228de2a252ccc0936494587
en301 <emilsjostrand1000@gmail.com>
2023-11-22 10:06:57 +0100

    Add project files.

---
+++ uppgift 5.3 E/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace uppgfit_5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Skrv in tal "+ (i+1) +" av 3.");
+                tal[i] = int.Parse(Console.ReadLine());
+            }
+            int sum = tal[0] + tal[1] + tal[2];
+            Console.WriteLine("Summan av talen blir "+ sum);
+            Console.ReadKey();
+        }
+    }
+}
